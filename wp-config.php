b2fab1e2bb0dc23a43267e87bfd5aa87570b9117
root <rewicken@gmail.com>
2016-11-30 21:13:38 +0000

    backup 11-30-2016

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'test');
+define('DB_NAME', 'famenycdata');
 
 /** MySQL database username */
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'test');
+define('DB_PASSWORD', 'wo8shimima');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
